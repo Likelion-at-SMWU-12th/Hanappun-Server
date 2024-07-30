4b114e6d62cdae0ace944e2b9c31bd1ec4d4e817
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 17:32:46 +0900

    Feat : 회원가입 뷰 구현

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSerializer, UserProfileSerializer
+from .models import User
 
 # Create your views here.
+
+# minseo : 회원 가입 뷰
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            
+            return Response({"message": "회원 가입이 완료 되었습니다. "}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
