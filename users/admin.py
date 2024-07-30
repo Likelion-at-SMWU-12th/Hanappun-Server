4b266bec29ec497d9439b7ba17dedbfbdcc1839f
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 16:03:35 +0900

    Feat: Users 모델 admin에 등록

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+# minseo : admin에 모델 등록
+@admin.register(User) 
+class PostModelAdmin(admin.ModelAdmin):
+    pass # 빈 클래스
