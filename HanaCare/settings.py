542321ecdaf1328a56ea7c376bee2a1619079396
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-30 16:27:38 +0900

    # minseo : AUTH 기본 모델을 Custom으로 변결

--- HanaCare/settings.py
+++ HanaCare/settings.py
@@ -109,6 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# minseo : AUTH 기본 모델을 Custom으로 변결
+AUTH_USER_MODEL = 'users.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
