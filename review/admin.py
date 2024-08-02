e7dd0fdeeb492f4924c931ea37684930e85066e9
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-02 20:28:16 +0900

    Feat : admin에 모델 등록

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+
+# minseo : admin에 모델 등록
+@admin.register(Review) 
+class PostModelAdmin(admin.ModelAdmin):
+    pass # 빈 클래스
