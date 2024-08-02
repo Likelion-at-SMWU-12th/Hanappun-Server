3d3d783944bcd66164945b59e7dcdbf32a615bcc
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-02 23:02:34 +0900

    Feat : 예약 모델 구현

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from users.models import User
+from clinic.models import Clinic
+
 # Create your models here.
+
+# minseo : 예약 모델
+class Reservation(models.Model):
+    client = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='환자 아이디')
+    date = models.DateTimeField(verbose_name="예약일정")
