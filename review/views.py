20b03488e42184dace997b846daf01fd2b106be1
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-02 20:29:14 +0900

    Feat : 리뷰 관련 api 구현

--- review/views.py
+++ review/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Count
+
+from .serializers import ReviewSerializer
+from .models import Review
 
 # Create your views here.
+
+# minseo : 리뷰 CRUD 뷰셋
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    # 한의원 별 리뷰 조회하기
+    filter_backends = [DjangoFilterBackend] 
+    filterset_fields = ['clinic']
+
+# minseo : 특정 한의원의 리뷰 카테고리 합계
+class CountReviewCategory(APIView):
+    def get(self, request, clinic_id):
+        review_counts = Review.objects.filter(clinic_id=clinic_id).values('review_cate').annotate(count=Count('review_cate'))
+        response_data = {item['review_cate']: item['count'] for item in review_counts}
+
+        return Response(response_data, status=status.HTTP_200_OK)
