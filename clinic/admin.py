50d7ee8c9f24bfdb9ce394c7688aa9af57f3264c
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-02 19:33:24 +0900

    Feat : admin에 모델 등록

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Clinic, ClinicImage
 
 # Register your models here.
+
+# minseo : admin에 모델 등록
+@admin.register(Clinic) 
+class PostModelAdmin(admin.ModelAdmin):
+    pass # 빈 클래스
+
+@admin.register(ClinicImage) 
+class PostModelAdmin(admin.ModelAdmin):
+    pass # 빈 클래스
