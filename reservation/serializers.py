9e1aa7d7931a5c253ffa5abfc73123d6c6f5b80f
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-05 13:59:51 +0900

    Feat : 예약 생성 시 clinic 정보도 뜨도록 수정

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
 from .models import Reservation
 from users.models import User
+from clinic.models import Clinic
 
 # minseo : 예약자의 나의 한의원 정보를 갖고오기
 class UserSerializer(ModelSerializer):
@@ -9,18 +10,18 @@ class UserSerializer(ModelSerializer):
         model = User
         fields = ['username', 'my_clinic'] 
 
-# minseo : 예약 
+# minseo : 예약 정보 조회
 class ReservationSerializer(ModelSerializer):
     client = UserSerializer()  
 
     class Meta:
         model = Reservation
-        fields = ['id', 'client', 'date']  
+        fields = ['id', 'client', 'date', 'clinic']
 
-# minseo : 예약 시에 
+# minseo : 예약 'POST', 'PUT', 'PATCH' 
 class ReservationCreateSerializer(ModelSerializer):
-    client = PrimaryKeyRelatedField(queryset=User.objects.all()) 
+    client = PrimaryKeyRelatedField(queryset=User.objects.all())
 
     class Meta:
         model = Reservation
-        fields = ['id', 'client', 'date']
+        fields = ['id', 'client', 'date', 'clinic']
--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import Response 
 from rest_framework import status
+
+from rest_framework.views import APIView
 
 from django_filters.rest_framework import DjangoFilterBackend
 from django.utils.dateparse import parse_date
@@ -9,6 +11,9 @@ from django.utils.dateparse import parse_date
 
 from .serializers import ReservationSerializer, ReservationCreateSerializer
 from .models import Reservation
+from users.models import User
+from clinic.models import Clinic
+from clinic.serializers import ClinicSerializer
 
 # Create your views here.
 
@@ -39,3 +44,35 @@ class ReservationByDateView(APIView):
         except Exception as error:
             return Response({"error": str(error)}, status=status.HTTP_400_BAD_REQUEST)
 
+
+class CreateReservation(APIView):
+    def post(self, request, format=None):
+        client_username = request.data.get('client')
+
+        try:
+            user = User.objects.get(username=client_username)
+        except User.DoesNotExist:
+            return Response({"message": "사용자를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        if user.my_clinic is None:
+            return Response({"message": "나의 한의원이 설정되어 있지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        data = request.data.copy()
+        data['clinic'] = user.my_clinic.id 
+        
+        serializer = ReservationCreateSerializer(data=data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"예약이 기록에 성공하였습니다","result" : serializer.data,}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, format=None):
+        pass
+
+    def delete(self, request, format=None):
+        pass
+
+
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['client']
