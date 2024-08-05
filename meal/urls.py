00f0bd8351bb4bedcb7a1ea68fd272af0fca02ff
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-06 03:58:46 +0900

    Fix : meal post 해결 2차

--- meal/urls.py
+++ meal/urls.py
@@ -3,5 +3,5 @@ from .views import MealAPIView
 
 urlpatterns = [
     path('meal/', MealAPIView.as_view(), name='meal_list_create_update_delete'),  # GET, POST, PUT, DELETE
-    path('meal/<str:date>/', MealAPIView.as_view(), name='meal_by_date'),  # GET
+    #path('meal/<str:date>/', MealAPIView.as_view(), name='meal_by_date'),  # GET
 ]
