2cb348a51b362bce019a04136d3637f6e97ab862
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-05 19:35:23 +0900

    Fix : 한의원 이미지 URL 필드로 변경

--- clinic/models.py
+++ clinic/models.py
@@ -11,6 +11,10 @@ class Clinic(models.Model):
     lat = models.FloatField("위도")
     lon = models.FloatField("경도")
     call = models.CharField("전화번호", max_length=50)
+
+    image_1 = models.URLField("한의원 사진 1", null=True, blank=True)
+    image_2 = models.URLField("한의원 사진 2", null=True, blank=True)
+    
     
     clinic_categories = (
         ('HealthCare', '건강 관리'),
@@ -26,22 +30,21 @@ class Clinic(models.Model):
     
 
 # minseo : 이미지 경로 지정 함수
-def clinic_image_upload_path(instance, filename):
-    return f'{instance.clinic.id}/clinic/{filename}'
+# def clinic_image_upload_path(instance, filename):
+#     return f'{instance.clinic.id}/clinic/{filename}'
 
-def doctor_image_upload_path(instance, filename):
-    return f'{instance.clinic.id}/doctor/{filename}'
+# def doctor_image_upload_path(instance, filename):
+#     return f'{instance.clinic.id}/doctor/{filename}'
 
-# minseo : 한의원 다중 이미지 정보 모델
-class ClinicImage(models.Model):
-    id = models.AutoField(primary_key=True)
-    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE, related_name='image')
-    image = models.ImageField(upload_to=clinic_image_upload_path)
+# # minseo : 한의원 다중 이미지 정보 모델
+# class ClinicImage(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE, related_name='image')
+#     image = models.ImageField(upload_to=clinic_image_upload_path)
 
 # minseo : 한의원 의사 정보 모델
 class Doctor(models.Model):
     name = models.CharField(max_length=15, verbose_name="의료진명")
     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE, verbose_name='한의원 아이디')
-    image = models.ImageField(upload_to=doctor_image_upload_path)
     profile = models.JSONField(blank=True, verbose_name="약력")
 
