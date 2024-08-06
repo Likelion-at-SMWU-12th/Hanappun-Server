4d9a0cb6ef134e83ad48b0c778e091274d6fe19d
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-06 18:47:54 +0900

    Feat : 식사 날짜별 조회 기능 구현

--- meal/urls.py
+++ meal/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import MealAPIView
+from .views import MealAPIView, ViewMealByDate
 
 urlpatterns = [
     path('meal/', MealAPIView.as_view(), name='meal_list_create_update_delete'),  # GET, POST, PUT, DELETE
-    #path('meal/<str:date>/', MealAPIView.as_view(), name='meal_by_date'),  # GET
+    path('date/meal/<str:date>/', ViewMealByDate.as_view(), name='meal_by_date'),  # GET
 ]
