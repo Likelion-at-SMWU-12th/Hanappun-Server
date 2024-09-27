35ee3b57d194f2da236e1bd3d3b077fe324b1d52
kyungminseo <110973127+mxinseo@users.noreply.github.com>
2024-09-28 07:16:43 +0900

    Update urls.py

--- meal/urls.py
+++ meal/urls.py
@@ -11,11 +11,10 @@
 # ]
 
 from django.urls import path
-from .views import MealListCreateAPIView, MealDetailByDateAPIView, DeleteMealDetailByDateAPIView, GetMealDataByID
+from .views import MealListCreateAPIView, MealDetailByDateAPIView, GetOrDeleteMealByidAPIView
 
 urlpatterns = [
     path('meal/<str:username>/', MealListCreateAPIView.as_view(), name='meal-list-create'),
     path('meal/<str:username>/date/', MealDetailByDateAPIView.as_view(), name='meal-detail-by-date'),  # 날짜 기반 수정 및 삭제
-    path('meal/<str:username>/<int:foodID>', DeleteMealDetailByDateAPIView.as_view(), name='meal-delete-by-name'),
-    path('meal/<str:username>/<int:id>', GetMealDataByID.as_view(), name='meal-list-create'),
+    path('meal/<str:username>/<int:foodID>', GetOrDeleteMealByidAPIView.as_view(), name='meal-get-delete-by-id'),
 ]
