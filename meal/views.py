077b6fc096464d19d41f1a698f55a8277f249cd4
SEOMUNJI <tjanswl@naver.com>
2024-09-27 22:55:54 +0900

    meal put 메뉴당 수정

--- meal/views.py
+++ meal/views.py
@@ -173,32 +173,40 @@ class MealDetailByDateAPIView(APIView):
         return Response(serializer.data)
     
     def put(self, request, username):
-        # URL에서 전달된 사용자 이름을 기반으로 사용자를 조회
         user = get_object_or_404(User, username=username)
-
-        # 날짜를 쿼리 파라미터로 받아서 해당 날짜의 기록을 업데이트
+        
         date_str = request.query_params.get('date', None)
         if date_str:
             try:
                 date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            # 해당 날짜와 사용자에 맞는 식사 기록 조회
                 meal = Meal.objects.filter(user=user, date=date).first()
                 if not meal:
-                    return Response({"error": f"No meals found for {date_str}."}, status=status.HTTP_404_NOT_FOUND)
+                    return Response({"error": f"{date_str}에 해당하는 식사 기록이 없습니다."}, status=status.HTTP_404_NOT_FOUND)
             except ValueError:
-                return Response({"error": "Invalid date format. Use YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "잘못된 날짜 형식입니다. YYYY-MM-DD 형식을 사용하세요."}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"error": "Date parameter is required."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "날짜 파라미터가 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # 쿼리 파라미터로 받은 날짜를 validated_data에 추가
-        data = request.data.copy()
-        data['date'] = date_str  # 날짜 필드를 추가
+    # request-body에서 'name'에 해당하는 메뉴를 업데이트
+        menu_name = request.data.get('name', None)
+        if menu_name:
+            meal_item = Meal.objects.filter(user=user, date=date, name=menu_name).first()
+            if not meal_item:
+                return Response({"error": f"{menu_name} 메뉴를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 쿼리 파라미터로 받은 날짜를 request 데이터에 추가
+            data = request.data.copy()
+            data['date'] = date_str  # 날짜 필드를 추가
 
         # user를 context에 포함시켜서 serializer로 전달
-        serializer = MealSerializer(meal, data=data, context={'user': user, 'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer = MealSerializer(meal_item, data=data, context={'user': user, 'request': request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+        return Response({"error": "'name' 필드가 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, username):
         user = get_object_or_404(User, username=username)
